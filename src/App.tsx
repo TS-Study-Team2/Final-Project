75d0863bd8c8af7d84aa839b2794edb1b8c3f5cb
se0jinYoon <tjwls1999@gmail.com>
2023-12-22 11:15:07 +0900

    init: 스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import styled, { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/globalStyle';
+import { theme } from './styles/theme';
+
 function App() {
   return (
-    <>
-      <div></div>
-      <h1>윤서진, 강민서 화이팅</h1>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Div>윤서진, 강민서 화이팅</Div>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+const Div = styled.div`
+  ${({ theme }) => theme.fonts.body03};
+`;
